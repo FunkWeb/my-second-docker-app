aea15ddcdcf1be9134ed93a598ad7eb1e3c5b700
edasa235 <edvardaslaksen130206@gmail.com>
2025-10-20 13:51:25 +0200

    feat: added api and docker

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -1,1 +1,32 @@
-// TODO: write controller for tasks
+import { Controller, Post, Body, Get, Param, HttpError } from 'routing-controllers';
+import { Task } from '../types/task.js';
+import crypto from 'crypto';
+import {tasksQueue} from "../queues/tasks.queue.js";
+
+@Controller('/tasks')
+export default class TaskController {
+  @Post('/')
+  async createTask(@Body() body: { title: string; description: string }) {
+    const task: Task = {
+      id: crypto.randomUUID(),
+      title: body.title,
+      description: body.description,
+      status: 'queued',
+      createdAt: new Date().toISOString(),
+    };
+
+    await tasksQueue.add('process-task', task, {
+      removeOnComplete: false,
+      removeOnFail: false,
+    });
+
+    return task;
+  }
+
+  @Get('/:id')
+  async getTask(@Param('id') id: string) {
+    const job = await tasksQueue.getJob(id);
+    if (!job) throw new HttpError(404, 'Not found');
+    return job.data as Task;
+  }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 4000;
 
 app.listen(port, () => {
   console.log(`✅ Server running on http://localhost:${port}`);
